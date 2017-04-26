a412296953b397e7448ad2f723a7bf06c4a366d9
Ollie Pai <ollie.pai2003@gmail.com>
2017-04-26 16:58:53 -0700

    Day 16

---
+++ src/NumberSorter.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class NumberSorter {
+	public static void main(String[] args) {
+		String x = JOptionPane.showInputDialog("Enter a number");
+		String y = JOptionPane.showInputDialog("Enter a number");
+		String z = JOptionPane.showInputDialog("Enter a number");
+		int a = Integer.parseInt(x);
+		int b = Integer.parseInt(y);
+		int c = Integer.parseInt(z);
+		if (a > b) {
+			if (a > c) {
+				System.out.println(a);
+			}
+		} else {
+			if (b > a) {
+				if (b > c) {
+					System.out.println(b);
+				}
+			} else {
+
+			}
+			if (c > a) {
+				if (c > b) {
+					System.out.println(c);
+				}
+			}
+		}
+		if (a < b) {
+			if (a > c) {
+				System.out.println(a);
+			}
+		} else {
+			if (b < a) {
+				if (b > c) {
+					System.out.println(b);
+				}
+			} else {
+
+			}
+			if (c < a) {
+				if (c > b) {
+					System.out.println(c);
+				}
+			}
+		}
+	}
+}
